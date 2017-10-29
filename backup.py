89bf1802eba2a78158d0825b771268862824694e
mauridb <mauriziocontatto@gmail.com>
2017-10-29 20:39:27 +0100

    :IMPROVEMENTS: create a specific folder splitting path and match folder zipped callname

--- backup.py
+++ backup.py
@@ -9,29 +9,41 @@ from datetime import datetime as dt
 # match passed params
 SCRIPTFILE = sys.argv[0]
 SRC_PATH = sys.argv[1]
+SRC_PATH_SPLITTED = SRC_PATH.split('/')
 DEST_PATH = sys.argv[2]
 ORIGIN_DIR = os.path.dirname(os.path.abspath(SCRIPTFILE))
 STORAGE_DIR = 'storage'
 DATE_FORMAT = '%d%m%Y-h%Hm%Ms%S'
 FULLPATH_STORAGE = DEST_PATH + STORAGE_DIR # full path of the storage zip folder
 
+print c('APP LAUNCHED..','green')
+
 # change dir in the passed destination path
 os.chdir(DEST_PATH)
 try:
 	# create standard dir folder 'storage'
 	os.mkdir(STORAGE_DIR, 0755)
-	print c('WELLDONE, CREATED SUCCESSFULLY!!', 'yellow')
+	print c('Welldone, storage folder created succesfully!!', 'yellow')
 except:
 	# handle OSError exception
-	print c('Folder already exists!!', 'yellow')
+	print c('Storage folder already exists!!', 'yellow')
 	pass
 
 os.chdir(STORAGE_DIR)
+specific_dir_name = '{}_BACKUPS'.format(SRC_PATH_SPLITTED[len(SRC_PATH_SPLITTED)-2]) # specific dir name
+try:
+	os.mkdir(specific_dir_name,0755)
+	print c('Welldone, specific folder created succesfully!!', 'yellow')
+except:
+	print c('Specific folder already exists!!', 'yellow')
 
+os.chdir(specific_dir_name)
+	
 print c('START: running compress mode','cyan')
 
 # create an archive file
-ZIP_NAME = 'WorkzoneBAK-{}'.format(dt.now().strftime(DATE_FORMAT))
+# ZIP_NAME = 'WorkzoneBAK-{}'.format(dt.now().strftime(DATE_FORMAT))
+ZIP_NAME = '{}BAK-{}'.format(SRC_PATH_SPLITTED[len(SRC_PATH_SPLITTED)-2],dt.now().strftime(DATE_FORMAT))
 try:
 	shutil.make_archive(ZIP_NAME,'zip', SRC_PATH, verbose=True)
 except:
@@ -41,6 +53,7 @@ except:
 
 
 print c('END: File compression completed!!', 'cyan')
+print c('APP TERMINATED CORRECTLY..','green')
 
 
 # print os.listdir('.') # or os.system('ls')
